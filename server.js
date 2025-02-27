b2a57974cd0234148325245b22c1ebeb53ba35ac
Jaskaran-creater <jaskaransingj6112@gmail.com>
2025-02-27 12:47:39 +0530

    server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+//import
+const express = require('express')
+
+//rest object
+const app = express();
+
+//routes
+app.get("/",(req, res) => {
+    res.send("<h1>welcome to  site</h1>")
+});
+
+//listen
+app.listen(8080, () => {
+        console.log("Node Server Running");
+    });
